0cd6c4af92878d2e1ffd54acb7cd5dceb8c4f440
Eric Hexter <eric.hexter@gmail.com>
2023-01-27 15:06:14 -0600

    Update certDP.sh

--- scripts/certDP.sh
+++ scripts/certDP.sh
@@ -15,7 +15,7 @@ echo "<?xml version='1.0' encoding='ASCII' standalone='yes'?>" > server.pref
 echo "<preferences>" >> server.pref
 echo "  <preference version=\"1\" name=\"cot_streams\">" >> server.pref
 echo "    <entry key=\"count\" class=\"class java.lang.Integer\">1</entry>" >> server.pref
-echo "    <entry key=\"description0\" class=\"class java.lang.String\">TAK Server (https://github.com/Cloud-RF/tak-server)</entry>" >> server.pref
+echo "    <entry key=\"description0\" class=\"class java.lang.String\">TAK Server </entry>" >> server.pref
 echo "    <entry key=\"enabled0\" class=\"class java.lang.Boolean\">true</entry>" >> server.pref
 echo "    <entry key=\"connectString0\" class=\"class java.lang.String\">$IP:8089:ssl</entry>" >> server.pref
 echo "  </preference>" >> server.pref
@@ -23,8 +23,8 @@ echo "  <preference version=\"1\" name=\"com.atakmap.app_preferences\">" >> serv
 echo "    <entry key=\"displayServerConnectionWidget\" class=\"class java.lang.Boolean\">true</entry>" >> server.pref
 echo "    <entry key=\"caLocation\" class=\"class java.lang.String\">cert/takserver.p12</entry>" >> server.pref
 echo "    <entry key=\"caPassword\" class=\"class java.lang.String\">atakatak</entry>" >> server.pref
-echo "    <entry key=\"clientPassword\" class=\"class java.lang.String\">atakatak</entry>" >> server.pref
 echo "    <entry key=\"certificateLocation\" class=\"class java.lang.String\">cert/$USER.p12</entry>" >> server.pref
+echo "    <entry key=\"clientPassword\" class=\"class java.lang.String\">atakatak</entry>" >> server.pref
 echo "  </preference>" >> server.pref
 echo "</preferences>" >> server.pref
 
@@ -38,12 +38,16 @@ echo "    <Parameter name=\"name\" value=\"$USER DP\"/>" >> manifest.xml
 echo "    <Parameter name=\"onReceiveDelete\" value=\"true\"/>" >> manifest.xml
 echo "  </Configuration>" >> manifest.xml
 echo "  <Contents>" >> manifest.xml
-echo "    <Content ignore=\"false\" zipEntry=\"certs\server.pref\"/>" >> manifest.xml
-echo "    <Content ignore=\"false\" zipEntry=\"certs\takserver.p12\"/>" >> manifest.xml
-echo "    <Content ignore=\"false\" zipEntry=\"certs\\$USER.p12\"/>" >> manifest.xml
+echo "    <Content ignore=\"false\" zipEntry=\"server.pref\"/>" >> manifest.xml
+echo "    <Content ignore=\"false\" zipEntry=\"takserver.p12\"/>" >> manifest.xml
+echo "    <Content ignore=\"false\" zipEntry=\"$USER.p12\"/>" >> manifest.xml
 echo "  </Contents>" >> manifest.xml
 echo "</MissionPackageManifest>" >> manifest.xml
+mkdir MANIFEST
+cp manifest.xml MANIFEST/manifest.xml
+zip -j tak/certs/files/$USER-$IP-itak.dp.zip manifest.xml server.pref tak/certs/files/takserver.p12 tak/certs/files/$USER.p12
+zip -j tak/certs/files/$USER-$IP-atak.dp.zip server.pref tak/certs/files/takserver.p12 tak/certs/files/$USER.p12
+zip -p tak/certs/files/$USER-$IP-atak.dp.zip MANIFEST\manifest.xml
 
-zip -j tak/certs/files/$USER-$IP.dp.zip manifest.xml server.pref tak/certs/files/takserver.p12 tak/certs/files/$USER.p12
 echo "-------------------------------------------------------------"
-echo "Created certificate data package for $USER @ $IP as tak/certs/files/$USER-$IP.dp.zip"
+echo "Created certificate data package for $USER @ $IP as tak/certs/files/$USER-$IP-itak.dp.zip"
